7521091ea7e80a1a49d47bdca12740ff85ac069c
Andre Sernajoto <andre.sernajoto@gmail.com>
2025-10-31 19:27:19 -0300

    feat: estilização do header e footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'wtw-violet': '#7f35b2'
+      }
+    },
   },
   plugins: [],
 }
